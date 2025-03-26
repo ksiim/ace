5020cb3ab86326abf5855863b7dfe8696ceb0be1
ksiim <makskrutchev@gmail.com>
2025-03-27 00:08:42 +0500

    added webhookpayload in transaction models

--- common/db/models/transaction.py
+++ common/db/models/transaction.py
@@ -40,3 +40,35 @@ class TransactionCreate(TransactionBase):
 class TransactionPublic(TransactionBase):
     id: int
     user_id: int
+    
+# class WebhookPayload(SQLModel):
+#     operation_id: str
+#     amount: float
+#     payment_type: str
+#     customer_code: str
+#     merchant_id: str
+#     purpose: str
+#     consumer_id: Optional[str]
+#     transaction_id: Optional[str]
+#     qrc_id: Optional[str]
+#     payer_name: Optional[str]
+
+class WebhookPayload(SQLModel, table=False):
+    operation_id: str = Field(..., alias="operationId", description="Идентификатор платежа")
+    amount: float = Field(..., description="Сумма платежа")
+    payment_type: str = Field(..., alias="paymentType", description="Тип платежа: 'card' или 'sbp'")
+    customer_code: Optional[str] = Field(None, alias="customerCode", description="Уникальный идентификатор клиента")
+    merchant_id: Optional[str] = Field(None, alias="merchantId", description="Идентификатор торговой точки")
+    purpose: str = Field(..., description="Назначение платежа")
+    consumer_id: Optional[str] = Field(None, alias="consumerId", description="Идентификатор покупателя (опционально для карты)")
+    transaction_id: Optional[str] = Field(None, alias="transactionId", description="Идентификатор платежа в СБП (только для sbp)")
+    qrc_id: Optional[str] = Field(None, alias="qrcId", description="Идентификатор QR-кода (только для sbp)")
+    payer_name: Optional[str] = Field(None, alias="payerName", description="Имя покупателя (только для sbp)")
+
+    class Config:
+        # Включаем использование alias для маппинга ключей из JWT
+        populate_by_name = True
+        from_attributes = True
+        # Разрешаем передачу данных с использованием alias (camelCase)
+        alias_generator = lambda field_name: field_name
+        
