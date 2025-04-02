86c5c690623bba657b1c1a1afeb692aa439a3a52
ksiim <makskrutchev@gmail.com>
2025-04-02 18:55:16 +0500

    added fio in participants list

--- frontend/src/pages/Tournament/TournamentPage.tsx
+++ frontend/src/pages/Tournament/TournamentPage.tsx
@@ -7,6 +7,11 @@ import ParticipantsList from './components/ParticipantsList/ParticipantsList.tsx
 import type { TournamentPage, User, Sex, Participant, Region } from './types.ts';
 import { Category } from '../Calendar/types.ts';
 
+// Расширяем тип Participant, добавляя birth_date
+interface ExtendedParticipant extends Participant {
+  birth_date?: string; // Предполагаем, что дата рождения приходит как строка
+}
+
 const TournamentPage: React.FC = () => {
   const { tournamentId } = useParams<{ tournamentId: string }>();
   const [tournament, setTournament] = useState<TournamentPage | null>(null);
@@ -17,23 +22,23 @@ const TournamentPage: React.FC = () => {
   const [userData, setUserData] = useState<User | null>(null);
   const [partnerId, setPartnerId] = useState<string>('');
   const [partnerData, setPartnerData] = useState<User | null>(null);
-  const [participants, setParticipants] = useState<Participant[]>([]);
+  const [participants, setParticipants] = useState<ExtendedParticipant[]>([]);
   const [sexes, setSexes] = useState<Sex[]>([]);
-  const [regions, setRegions] = useState<Region[]>([])
+  const [regions, setRegions] = useState<Region[]>([]);
   const [categories, setCategories] = useState<Category[]>([]);
   const navigate = useNavigate();
-  
+
   const loadParticipants = async () => {
     if (!tournament || !userData) return;
-    
+
     try {
       const response = await apiRequest(`tournaments/${tournament.id}/participants`, 'GET', undefined, false);
       if (response && !response.error) {
         setParticipants(response.data);
-        
+
         // Проверяем, зарегистрирован ли текущий пользователь
         const isUserRegistered = response.data.some(
-          (participant: Participant) => participant.user_id === userData.id
+          (participant: ExtendedParticipant) => participant.user_id === userData.id
         );
         setIsRegistered(isUserRegistered);
       }
@@ -41,14 +46,14 @@ const TournamentPage: React.FC = () => {
       console.error('Ошибка при загрузке списка участников:', error);
     }
   };
-  
+
   useEffect(() => {
     const fetchTournament = async () => {
       try {
         const response = await apiRequest(`tournaments/${tournamentId}`, 'GET', undefined, false);
         if (response) {
           setTournament(response);
-          
+
           // Загружаем участников сразу после загрузки турнира
           const participantsResponse = await apiRequest(
             `tournaments/${response.id}/participants`,
@@ -58,11 +63,11 @@ const TournamentPage: React.FC = () => {
           );
           if (participantsResponse && !participantsResponse.error) {
             setParticipants(participantsResponse.data);
-            
+
             // Проверяем, зарегистрирован ли текущий пользователь
             if (userData) {
               const isUserRegistered = participantsResponse.data.some(
-                (participant: Participant) => participant.user_id === userData.id
+                (participant: ExtendedParticipant) => participant.user_id === userData.id
               );
               setIsRegistered(isUserRegistered);
             }
@@ -75,7 +80,6 @@ const TournamentPage: React.FC = () => {
       }
     };
 
-    
     const fetchSexes = async () => {
       try {
         const response = await apiRequest('sex/', 'GET', undefined, false);
@@ -86,11 +90,11 @@ const TournamentPage: React.FC = () => {
         console.error('Ошибка загрузки списка полов:', error);
       }
     };
-    
+
     fetchSexes();
     fetchTournament();
-  }, [tournamentId, userData]); // Добавляем userData в зависимости
-  
+  }, [tournamentId, userData]);
+
   useEffect(() => {
     if (tournament) {
       loadParticipants();
@@ -108,7 +112,7 @@ const TournamentPage: React.FC = () => {
 
     fetchCategories();
   }, []);
-  
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
@@ -118,10 +122,10 @@ const TournamentPage: React.FC = () => {
         console.error('Ошибка при загрузке данных о пользователе:', error);
       }
     };
-    
+
     fetchUserData();
   }, []);
-  
+
   useEffect(() => {
     const fetchRegions = async () => {
       try {
@@ -130,43 +134,42 @@ const TournamentPage: React.FC = () => {
           setRegions(response.data);
         }
       } catch (error) {
-        console.error("Ошибка при загрузке регионов");
+        console.error('Ошибка при загрузке регионов');
       }
     };
-    
+
     fetchRegions();
   }, []);
-  
+
   useEffect(() => {
     if (!tournament) return; // Проверка, что tournament не null
-    
+
     const tournamentDate = new Date(tournament.date);
     const today = new Date();
-    
+
     // Убираем время из дат для корректного сравнения
     tournamentDate.setHours(0, 0, 0, 0);
     today.setHours(0, 0, 0, 0);
-    
+
     // Вычисляем разницу в днях между датой турнира и текущей датой
     const diffTime = tournamentDate.getTime() - today.getTime();
     const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-    
+
     // Проверяем, прошла ли дата турнира
     const isTournamentInPast = tournamentDate < today;
-    
+
     // Проверяем, осталось ли до турнира больше 30 дней
     const isRegistrationClosedDueToTime = diffDays > 30;
-    
+
     // Если турнир уже прошел или до него осталось больше 30 дней, закрываем регистрацию
     if ((isTournamentInPast || isRegistrationClosedDueToTime) && tournament.can_register) {
       setTournament((prev) => {
         if (!prev) return prev; // Защита от null
-        
         return { ...prev, can_register: false } as TournamentPage; // Приведение к нужному типу
       });
     }
   }, [tournament]);
-  
+
   const handleRegisterClick = () => {
     setIsModalOpen(true);
   };
@@ -176,29 +179,29 @@ const TournamentPage: React.FC = () => {
     const category = categories.find((cat) => cat.id === categoryId);
     return category ? category.name : 'Не указана';
   };
-  
+
   const handleConfirmRegistration = async () => {
     setIsRegistering(true);
     try {
       if (userData && tournament) {
         // Получаем участников турнира
         const participantsResponse = await apiRequest(`tournaments/${tournament.id}/participants`, 'GET', undefined, false);
-        
+
         // Ищем участие текущего пользователя
         const existingParticipant = participantsResponse.data.find(
           (participant: any) => participant.user_id === userData.id
         );
-        
+
         // Проверка, является ли текущий пользователь партнёром кого-либо
         const isAlreadyPartner = participantsResponse.data.some(
           (participant: any) => participant.partner_id === userData.id
         );
-        
+
         if (isAlreadyPartner) {
           alert('Вы уже являетесь партнёром другого участника!');
           return; // Прекращаем дальнейшее выполнение
         }
-        
+
         // Данные для регистрации или обновления
         const registrationData = {
           tournament_id: tournament.id,
@@ -208,7 +211,7 @@ const TournamentPage: React.FC = () => {
             ? `${userData.surname} ${userData.name} ${userData.patronymic}\ ${partnerData?.surname} ${partnerData?.name} ${partnerData?.patronymic}`
             : `${userData.surname} ${userData.name} ${userData.patronymic}`,
         };
-        
+
         if (existingParticipant) {
           // Если участие уже существует, обновляем его, но сохраняем текущее значение confirmed
           const participantId = existingParticipant.id;
@@ -221,7 +224,7 @@ const TournamentPage: React.FC = () => {
             },
             true
           );
-          
+
           if (!updateResponse.error) {
             alert('Ваше участие в турнире обновлено!');
             await loadParticipants(); // Перезагружаем список участников
@@ -243,15 +246,15 @@ const TournamentPage: React.FC = () => {
             alert('Вы успешно зарегистрировались на турнир!');
             await loadParticipants(); // Перезагружаем список участников
           } else {
-            navigate('/subscription')
+            navigate('/subscription');
           }
         }
-        
+
         setIsModalOpen(false);
       }
     } catch (error: any) {
       console.error('Ошибка при отправке запроса на регистрацию:', error);
-  
+
       // Проверяем статус ошибки
       if (error.response && error.response.status === 403) {
         // Перенаправляем на страницу /subscription
@@ -263,15 +266,15 @@ const TournamentPage: React.FC = () => {
       setIsRegistering(false);
     }
   };
-  
+
   const handleCancelRegistration = () => {
     setIsModalOpen(false);
   };
-  
+
   const handlePartnerIdChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
     const partnerId = event.target.value;
     setPartnerId(partnerId);
-    
+
     // Если партнёр добавлен, получаем его данные
     if (partnerId) {
       try {
@@ -289,7 +292,7 @@ const TournamentPage: React.FC = () => {
       setPartnerData(null); // Сбрасываем данные, если поле пустое
     }
   };
-  
+
   const renderContent = () => {
     if (loading) {
       return (
@@ -299,11 +302,11 @@ const TournamentPage: React.FC = () => {
         </div>
       );
     }
-    
+
     if (!tournament) {
       return <div className={styles.error}>Турнир не найден</div>;
     }
-    
+
     return (
       <div className={styles.tournamentContainer}>
         <div className={styles.contentWrapper}>
@@ -315,7 +318,10 @@ const TournamentPage: React.FC = () => {
             <p className={styles.tournamentType}>
               <strong>Тип турнира:</strong> {tournament.type === 'solo' ? 'Одиночный' : 'Парный'}
             </p>
-            <p className={styles.tournamentRegion}> <strong>Регион:</strong> {regions.find(region => region.id === tournament.region_id)?.name || 'Не указан'}</p>
+            <p className={styles.tournamentRegion}>
+              <strong>Регион:</strong>{' '}
+              {regions.find((region) => region.id === tournament.region_id)?.name || 'Не указан'}
+            </p>
             <p className={styles.tournamentAddress}>
               <strong>Категория:</strong> {getCategoryNameById(tournament.category_id)}
             </p>
@@ -334,7 +340,8 @@ const TournamentPage: React.FC = () => {
               </p>
             )}
             <p className={styles.tournamentPrice}>
-              <strong>Стоимость участия (за человека):</strong> {tournament.price > 0 ? `${tournament.price} ₽` : 'Бесплатно'}
+              <strong>Стоимость участия (за человека):</strong>{' '}
+              {tournament.price > 0 ? `${tournament.price} ₽` : 'Бесплатно'}
             </p>
             {tournament.prize_fund && (
               <p className={styles.tournamentPrizeFund}>
@@ -342,16 +349,14 @@ const TournamentPage: React.FC = () => {
               </p>
             )}
             <p className={styles.tournamentSex}>
-              <strong>Пол участников:</strong> {sexes.find(sex => sex.id === tournament.sex_id)?.name || 'Не указан'}
+              <strong>Пол участников:</strong>{' '}
+              {sexes.find((sex) => sex.id === tournament.sex_id)?.name || 'Не указан'}
             </p>
             <div className={styles.registrationContainer}>
               {tournament.can_register && (
                 <>
                   {tournament.type === 'solo' && isRegistered ? null : (
-                    <button
-                      className={styles.registrationButton}
-                      onClick={handleRegisterClick}
-                    >
+                    <button className={styles.registrationButton} onClick={handleRegisterClick}>
                       {isRegistered ? 'Обновить участие' : 'Зарегистрироваться'}
                     </button>
                   )}
@@ -362,7 +367,7 @@ const TournamentPage: React.FC = () => {
               )}
             </div>
           </div>
-          
+
           {tournament.photo_path && (
             <div className={styles.imageContainer}>
               <img
@@ -373,20 +378,18 @@ const TournamentPage: React.FC = () => {
             </div>
           )}
         </div>
-        
-        <ParticipantsList
-          participants={participants}
-        />
+
+        <ParticipantsList participants={participants} />
       </div>
     );
   };
-  
+
   return (
     <div className={styles.pageWrapper}>
       <Header scrollToBenefits={() => {}} />
       <main className={styles.mainContent}>
         {renderContent()}
-        
+
         {isModalOpen && (
           <div className={styles.modal}>
             <div className={styles.modalContent}>
@@ -402,7 +405,9 @@ const TournamentPage: React.FC = () => {
                     placeholder="Введите ID партнёра"
                   />
                   {partnerData && partnerData.surname && partnerData.name && partnerData.patronymic && (
-                    <p>Партнёр: {partnerData.surname} {partnerData.name} {partnerData.patronymic}</p>
+                    <p>
+                      Партнёр: {partnerData.surname} {partnerData.name} {partnerData.patronymic}
+                    </p>
                   )}
                 </div>
               )}
--- frontend/src/pages/Tournament/components/ParticipantsList/ParticipantsList.tsx
+++ frontend/src/pages/Tournament/components/ParticipantsList/ParticipantsList.tsx
@@ -1,21 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { apiRequest } from "../../../../utils/apiRequest.ts";
 import styles from "./ParticipantsList.module.scss";
-import {Check, Clock} from 'lucide-react';
-import {ParticipantsListProps, Fio} from '../../types.ts';
+import { Check, Clock } from 'lucide-react';
+import { ParticipantsListProps, Fio } from '../../types.ts';
 
+// Расширяем интерфейс Fio, добавляя birth_date
+interface ExtendedFio extends Fio {
+  birth_date?: string; // Предполагаем, что дата рождения приходит как строка
+}
 
 const ParticipantsList: React.FC<ParticipantsListProps> = ({
-                                                             participants,
-                                                           }) => {
-  const [userDetails, setUserDetails] = useState<Record<number, Fio>>({});
-  
+  participants,
+}) => {
+  const [userDetails, setUserDetails] = useState<Record<number, ExtendedFio>>({});
+
+  // Функция для форматирования даты в "10 января 2006г."
+  const formatDate = (dateString?: string) => {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    return date.toLocaleDateString('ru-RU', {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    });
+  };
+
   useEffect(() => {
     // Загружаем информацию о пользователях при изменении списка участников
     participants.forEach(participant => {
       // Загружаем данные о пользователе
       if (participant.user_id && !userDetails[participant.user_id]) {
-        apiRequest(`users/${participant.user_id}/fio`, "GET", undefined, false)
+        apiRequest(`users/${participant.user_id}/`, "GET", undefined, false)
           .then(response => {
             if (!response.error) {
               setUserDetails(prevDetails => ({
@@ -23,17 +38,18 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({
                 [participant.user_id]: {
                   name: response.name,
                   surname: response.surname,
-                  patronymic: response.patronymic // Добавляем отчество
+                  patronymic: response.patronymic, // Добавляем отчество
+                  birth_date: response.birth_date, // Добавляем дату рождения
                 },
               }));
             }
           })
           .catch(error => console.error("Ошибка запроса данных пользователя:", error));
       }
-      
+
       // Загружаем данные о партнёре, только если partner_id не null
       if (participant.partner_id !== null && !userDetails[participant.partner_id]) {
-        apiRequest(`users/${participant.partner_id}/fio`, "GET", undefined, false)
+        apiRequest(`users/${participant.partner_id}/`, "GET", undefined, false)
           .then(response => {
             if (!response.error) {
               setUserDetails(prevDetails => ({
@@ -41,7 +57,8 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({
                 [participant.partner_id as number]: {
                   name: response.name,
                   surname: response.surname,
-                  patronymic: response.patronymic // Добавляем отчество
+                  patronymic: response.patronymic, // Добавляем отчество
+                  birth_date: response.birth_date, // Добавляем дату рождения
                 },
               }));
             }
@@ -50,9 +67,7 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({
       }
     });
   }, [participants, userDetails]);
-  
-  
-  
+
   return (
     <div className={styles.tournamentParticipants}>
       <h3>Список участников</h3>
@@ -63,24 +78,39 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({
           {participants.map(participant => (
             <li key={participant.id} className={styles.participantItem}>
               <span className={styles.participantName}>
-                {userDetails[participant.user_id]?.surname} {userDetails[participant.user_id]?.name}
-                {userDetails[participant.user_id]?.patronymic && (
-                  <> {userDetails[participant.user_id]?.patronymic}</>
-                )}
-                {participant.partner_id !== null && userDetails[participant.partner_id] && (
-                  <> / {userDetails[participant.partner_id]?.surname} {userDetails[participant.partner_id]?.name}
-                    {userDetails[participant.partner_id]?.patronymic && (
-                      <> {userDetails[participant.partner_id]?.patronymic}</>
-                    )}
-                  </>
-                )}
+                <div>
+                  {userDetails[participant.user_id]?.surname || ''}{' '}
+                  {userDetails[participant.user_id]?.name || ''}
+                  {userDetails[participant.user_id]?.patronymic && (
+                    <> {userDetails[participant.user_id]?.patronymic}</>
+                  )}
+                  {participant.partner_id !== null && userDetails[participant.partner_id] && (
+                    <> / {userDetails[participant.partner_id]?.surname || ''}{' '}
+                      {userDetails[participant.partner_id]?.name || ''}
+                      {userDetails[participant.partner_id]?.patronymic && (
+                        <> {userDetails[participant.partner_id]?.patronymic}</>
+                      )}
+                    </>
+                  )}
+                </div>
+                <div>
+                  {userDetails[participant.user_id]?.birth_date && (
+                    <>{formatDate(userDetails[participant.user_id]?.birth_date)}</>
+                  )}
+                  {participant.partner_id !== null && userDetails[participant.partner_id]?.birth_date && (
+                    <> / {formatDate(userDetails[participant.partner_id]?.birth_date)}</>
+                  )}
+                </div>
               </span>
               {participant.confirmed ? (
-                <span
-                  className={`${styles.participantStatus} ${styles.confirmed}`}><Check/></span>
-              ) : <span
-                className={`${styles.participantStatus} ${styles.confirmed}`}><Clock color={'#f95e1b'}/></span>
-                }
+                <span className={`${styles.participantStatus} ${styles.confirmed}`}>
+                  <Check />
+                </span>
+              ) : (
+                <span className={`${styles.participantStatus} ${styles.confirmed}`}>
+                  <Clock color={'#f95e1b'} />
+                </span>
+              )}
             </li>
           ))}
         </ul>
@@ -88,6 +118,5 @@ const ParticipantsList: React.FC<ParticipantsListProps> = ({
     </div>
   );
 };
-
 
 export default ParticipantsList;
