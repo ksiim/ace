3dca65740c5b8638790ec78b01e46c234736bb12
ksiim <makskrutchev@gmail.com>
2025-04-24 18:53:29 +0500

    added info in footer

--- frontend/src/components/Footer/Footer.tsx
+++ frontend/src/components/Footer/Footer.tsx
@@ -12,18 +12,40 @@ const Footer: React.FC = () => {
   
   return (
     <footer className={styles.wrapper}>
-      <div className={styles.content}>
-        <div className={styles.logo}>
-          ACE — платформа для развития<br/>
-          детского тенниса.
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.logo}>
+            ACE — платформа для развития<br/>
+            детского тенниса.
+          </div>
+          
+          <button
+            onClick={scrollToTop}
+            className={styles.scroll_top}
+          >
+            Вернуться наверх <ChevronUp className={styles.icon} />
+          </button>
         </div>
         
-        <button
-          onClick={scrollToTop}
-          className={styles.scroll_top}
-        >
-          Вернуться наверх <ChevronUp className={styles.icon} />
-        </button>
+        <div className={styles.legal_info}>
+          <p>ИП Донченко Р.В.</p>
+          <p>ИНН 616116698853</p>
+          <p>ОГРН 324619600133530 от 14 июня 2024г.</p>
+          <p>Адрес: Ростовская область, Аксайский р-он, ст-ца Старочеркасская, пер. Пляжный, 13.</p>
+          <p>Электронная почта: <a href="mailto:ace.tour@mail.ru">ace.tour@mail.ru</a></p>
+        </div>
+        
+        <div className={styles.documents}>
+          <a href="https://ace-deti.ru/uploads/c0e4f7f2-0c74-4f70-83dc-1fc3ae87a8ac.pdf" target="_blank" rel="noopener noreferrer">
+            Политика конфиденциальности
+          </a>
+          <a href="https://ace-deti.ru/uploads/ed19f5ac-6b6c-4fea-b3d8-ed5ad5deacb5.pdf" target="_blank" rel="noopener noreferrer">
+            Пользовательское соглашение
+          </a>
+          <a href="https://ace-deti.ru/uploads/122e6599-0fab-4d13-bf09-9e53c43f887a.pdf" target="_blank" rel="noopener noreferrer">
+            Положение о платформе Эйс
+          </a>
+        </div>
       </div>
     </footer>
   );
