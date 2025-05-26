29da0a795c1121bc39662b9ef259c7b86865e835
ksiim <makskrutchev@gmail.com>
2025-05-26 14:29:06 +0500

    date sort default value DESC

--- backend/app/api/routes/tournaments.py
+++ backend/app/api/routes/tournaments.py
@@ -106,7 +106,7 @@ async def read_all_tournaments(
     category_id: Optional[int] = None,
     sex_id: Optional[int] = None,
     actual: Optional[bool] = None,
-    date_sort: Optional[OrderEnum] = OrderEnum.ASC,
+    date_sort: Optional[OrderEnum] = OrderEnum.DESC,
     type: Optional[TournamentType] = None,  # Новый фильтр по типу
 ) -> Any:
     """
